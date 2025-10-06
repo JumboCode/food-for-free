3ce23f236a6ac10c78b8af40bf598fdd8c48ae5f
jpaik03 <justin.paik@tufts.edu>
2025-10-06 10:59:18 -0400

    created Justin's button

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -4,11 +4,17 @@ import React from "react";
 
 // TODO: add what the button takes in
 type ButtonProps = {
-  
+  label: string;
+  onClick: () => void;
 };
 
-export default function Button({ /* add to this */ }: ButtonProps) {
+export default function Button({ label, onClick }: ButtonProps) {
   return (
-    // TODO: create the button
+    <button
+      onClick={onClick}
+      className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-xl shadow-md transition"
+    >
+      {label}
+    </button>
   );
 }
--- src/app/weather/page.tsx
+++ src/app/weather/page.tsx
@@ -10,12 +10,27 @@ export default function WeatherPage() {
   // TODO: Fetch weather data from an API
   const getWeather = async () => {
     // Hint: call the API, extract some info, and update state with setWeather(...)
+    const api_key = process.env.NEXT_PUBLIC_WEATHER_KEY;
+    try {
+      const res = await fetch(
+        `http://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=02130&days=1&aqi=no&alerts=no`
+      );
+      const data = await res.json();
+      const temp = data.current.temp_c;
+      setWeather(`Current temperature: ${temp}°C`);
+    } catch (err) {
+      setWeather("Failed.");
+    }
   };
 
   return (
     <div>
       {/* TODO: Create an instance of the Button component that calls getWeather when clicked */}
       {/* TODO: Display the weather info here */}
+      <div className="flex flex-col items-center gap-4 p-6">
+      <Button label="Get Weather" onClick={getWeather} />
+      {weather && <p>{weather}</p>}
+    </div>
     </div>
   );
 }
