d2696b95f2001571bca52b2f54ffcf5312ec545c
Julia Yoo <julia.mfy@gmail.com>
2025-10-16 09:54:33 -0400

    commented and add note fixed

--- src/app/note/page.tsx
+++ src/app/note/page.tsx
@@ -1,47 +1,52 @@
-// Full Stack Weather Button Starter Code 
 
 "use client";
 import { useState } from "react";
 import Note from "../components/notes";
 
+// Note Component Function 
 export default function NotePage() {
     const [notes, setNotes] = useState<string[]>([]);
     const [currNote, setCurrNote] = useState<string>("");
-
+// Add a note function 
     const addNote = (note: string) => {
+        // append string to current note 
         setNotes([...notes, currNote]);
-        setCurrNote("");
-        console.log("note saved");
+        console.log("note saved:", currNote);
+    
     };
+    // clear the current note and the saved notes
     const clearNote = () =>{
-                console.log("cleared: ", currNote)
+        console.log("cleared: ", currNote)
 
         setCurrNote("");
         setNotes([]);
     }
 
   return (
-    <div>
-
-    <h1 style={{ fontFamily: "Arial, sans-serif", fontSize: "24px", }}>Notes:</h1>
-      
+    <div style = {{margin: "20px"}}>
+         {/* header */}
+    <h1 style={{ fontFamily: "Arial, sans-serif", fontSize: "24px"}}>Notes:</h1>
+         {/* note component */}
         <div className="flex flex-col">
         <Note text={currNote} onChange={(val) => setCurrNote(val)} />
 
         </div>
-      
+        {/* save button */}
        <button
         className="Save" style={{
         border: "black 1px solid",
         borderRadius: "8px",
         padding: "5px 5px",
+        width: "60px",
        }}
        onClick={() => addNote(currNote)}>Save</button>
-
+        {/* clear button */}
        <button className="Clear" style={{
         border: "black 1px solid",
         borderRadius: "8px",
         padding: "5px 5px",
+        width: "60px",
+
        }}
        onClick={() =>clearNote()}>Clear</button>
     </div>
