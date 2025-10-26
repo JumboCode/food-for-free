74318a21a1a6c47a9c4b3317b041051adab8ca3c
Benjamin Li <benjamin.mingjun@gmail.com>
2025-10-26 16:23:08 -0400

    removed createbody

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,26 +1,20 @@
 // app/api/messages/route.ts
 
-import { NextResponse } from "next/server";
-import prisma from "../../../../lib/prisma";
-
-type CreateBody = {
-  text?: unknown;
-};
+import { NextResponse } from 'next/server';
+import prisma from '../../../../lib/prisma';
 
 export async function POST(req: Request) {
-
-  try {
-    // FILL THIS IN
-  } catch (err) {
-    // FILL THIS IN
-  }
+    try {
+        // FILL THIS IN
+    } catch (err) {
+        // FILL THIS IN
+    }
 }
 
 export async function GET() {
-  try {
-    // FILL THIS IN
-  } catch (err) {
-    // FILL THIS IN
-  }
+    try {
+        // FILL THIS IN
+    } catch (err) {
+        // FILL THIS IN
+    }
 }
-
