ffb9caf66955499ff00636018289d6a933f72681
Riddhi Sahni <riddhisahni44@gmail.com>
2025-10-26 16:20:58 -0400

    Starter Code

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+<<<<<<< Updated upstream
   /* config options here */
+=======
+  eslint: {
+    // disable ESLint during builds
+    ignoreDuringBuilds: true,
+  },
+  // Ensures file-tracing works correctly if you're using a monorepo or custom layout
+  outputFileTracingRoot: __dirname,
+>>>>>>> Stashed changes
 };
 
 export default nextConfig;
---
+++ src/app/api/message/route.ts
@@ -0,0 +1,26 @@
+// app/api/messages/route.ts
+
+import { NextResponse } from "next/server";
+import prisma from "../../../../lib/prisma";
+
+type CreateBody = {
+  text?: unknown;
+};
+
+export async function POST(req: Request) {
+
+  try {
+    // FILL THIS IN
+  } catch (err) {
+    // FILL THIS IN
+  }
+}
+
+export async function GET() {
+  try {
+    // FILL THIS IN
+  } catch (err) {
+    // FILL THIS IN
+  }
+}
+
