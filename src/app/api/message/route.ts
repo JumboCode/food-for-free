1bececed33a05b507edab72c6d8ac62c2d0d600e
Riddhi Sahni <riddhisahni44@gmail.com>
2025-10-26 18:38:20 -0400

    Finalized starter code

---
+++ src/app/api/message/route.ts
@@ -0,0 +1,22 @@
+// app/api/messages/route.ts
+
+import { NextResponse } from "next/server";
+import prisma from "../../../../lib/prisma";
+
+export async function POST(req: Request) {
+
+  try {
+    // add code here
+  } catch (err) {
+    // add code here
+  }
+}
+
+export async function GET() {
+  try {
+    // add code here
+  } catch (err) {
+    // add code here
+  }
+}
+
