1e47d877bb2000635ca44956f3df8e71358b3329
Riddhi Sahni <riddhisahni44@gmail.com>
2025-11-15 16:12:27 -0500

    Formatting updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import {
 } from '@clerk/nextjs';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import SideNavBar from '@/components/ui/SideNavbar';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -27,27 +28,36 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
     children,
-}: Readonly<{
+}: {
     children: React.ReactNode;
-}>) {
+}) {
     return (
         <ClerkProvider>
             <html lang="en">
                 <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                    <header className="flex justify-end items-center p-4 gap-4 h-16">
-                        <SignedOut>
-                            <SignInButton />
-                            <SignUpButton>
-                                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                                    Sign Up
-                                </button>
-                            </SignUpButton>
-                        </SignedOut>
-                        <SignedIn>
-                            <UserButton />
-                        </SignedIn>
-                    </header>
-                    {children}
+                    
+                    <SideNavBar />
+
+                    <div className="ml-16 sm:ml-56">
+
+                        <header className="flex justify-end items-center p-4 gap-4 h-16">
+                            <SignedOut>
+                                <SignInButton />
+                                <SignUpButton>
+                                    <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                                        Sign Up
+                                    </button>
+                                </SignUpButton>
+                            </SignedOut>
+                            <SignedIn>
+                                <UserButton />
+                            </SignedIn>
+                        </header>
+
+                        <main className="p-6">
+                            {children}
+                        </main>
+                    </div>
                 </body>
             </html>
         </ClerkProvider>
